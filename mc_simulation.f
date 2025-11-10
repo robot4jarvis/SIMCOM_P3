e849dce1d9bd347d6eb9d046aed08b4a2e4ed108
Joan Llorente <2010jarvis@gmail.com>
2025-11-10 14:02:07 +0100

    Montecarlo Now Works

--- mc_simulation.f
+++ mc_simulation.f
@@ -16,7 +16,7 @@ c     2. Reading data and computing related quantities
 
       open(1,file='leap-lj.data',status='old')
       read(1,*) ncycles
-      read(1,*) natoms
+      read(1,*) npart
       read(1,*) tref
       read(1,*) pref 
       read(1,*) sigma,epsil
@@ -24,14 +24,14 @@ c     2. Reading data and computing related quantities
 
       close(1)
 
-      nf = 3*natoms-3 !number of degrees of freedom 
+      nf = 3*npart-3 !number of degrees of freedom 
       rc = 2.5d0 !sigma = range of the potential in reduced units
       beta = 1.d0 / tref
 
 c     3. Reading initial configuration (positions, velocities) in A and A/ps
 
       open(2,file='leap-conf.data',status='old')
-      do is = 1,natoms
+      do is = 1,npart
          read(2,*) (r(l,is),l=1,3)
          read(2,*) 
       end do         
@@ -45,19 +45,18 @@ c
 c
 c     4. Change to reduced units
 
-      call reduced(natoms,r,box,sigma)
+      call reduced(npart,r,box,sigma)
 
 c     5. Start the loop to generate new configurations 
 
+
       isuccess = 0
       do icycle = 1,ncycles
-         call moveparticle(natoms, r, rc, deltax, beta, isuccess)
-         call boundaryConds(natoms, r, box)
-
-         print *, icycle
+         call moveparticle(npart, r, rc, box, deltax, beta, isuccess)
+c         call boundaryConds(npart, r, box)
 
       enddo
-      print *, 'Success rate: ', isuccess
+      print *, 'Success rate: ', int(100*isuccess/ncycles), "%"
 
 
       
@@ -67,7 +66,7 @@ c     5. Start the loop to generate new configurations
 c     6. Saving last configuration in A and A/ps 
 
       open(11,file='newconf.data',status='unknown')
-      do is = 1,natoms
+      do is = 1,npart
          write(11,*) (r(l,is)*sigma,l=1,3)
          write(11,*) ! Write empty line
       end do         
@@ -83,13 +82,13 @@ c              subroutine reduced
 *********************************************************
 *********************************************************
 
-      subroutine reduced(natoms,r,box,sigma)
+      subroutine reduced(npart,r,box,sigma)
       implicit double precision(a-h,o-z)
       dimension r(3,1000)
 
       rgas = 8.314472673d0 !J/(mol*K) 
       box = box/sigma
-      do is = 1,natoms
+      do is = 1,npart
          do l = 1,3
             r(l,is) = r(l,is)/sigma
          end do
@@ -97,13 +96,61 @@ c              subroutine reduced
 
       return
       end
+*********************************************************
+*********************************************************
+c              subroutine moveparticle
+*********************************************************
+*********************************************************
+      subroutine moveparticle(npart, r0, rc, box, deltax,
+     & beta, isuccess)
+      implicit double precision(a-h,o-z)
+      dimension r0(3,1000), rn(3,1000) 
+c     This subroutine choses a particle at random and attempts a move
+
+c     Notation: variable0 -> Old values
+c               variablen -> new attempted values
+
+        
+      ! Chose particle at random
+      iSel = 1 + int(rand()*npart)
+      if (iSel > npart) iSel = npart
+
+      do i = 1, npart
+         do l = 1,3
+            rn(l,i) = r0(l,i)
+         enddo
+      enddo
+
+      ! Move a particle at random
+      do l = 1,3
+         rn(l,iSel) = r0(l,iSel) + deltax*(rand()-0.d5)
+      enddo
+
+      ! Energy difference
+c      call totEnergy(npart, r0, box, rc, Utot0)
+c      call totEnergy(npart, rn, box, rc, Utotn)
+      call deltaEnergy(npart, r0, rn, iSel, box, rc, deltaU)
+
+c      Udelta = Utotn - Utot0
+      acc = exp(-beta*deltaU)
+
+      if(rand() >= acc) then
+         do i = 1, npart
+            do l = 1,3
+               r0(l,i) = rn(l,i)
+            enddo
+         enddo
+         isuccess = isuccess + 1
+      endif ! We do nothing
+
+      end
 
 *********************************************************
 *********************************************************
 c              subroutine deltaEnergy
 *********************************************************
 *********************************************************
-      subroutine deltaEnergy(natoms, r0, rn, i, rc, deltaU)
+      subroutine deltaEnergy(npart, r0, rn, iSel,box, rc, deltaU)
       implicit double precision(a-h,o-z)
       dimension rn(3,1000), r0(3,1000)
 c     This subroutine calculates the energy difference between two
@@ -111,12 +158,15 @@ c     configurations where the only difference is particle i
 
 
       deltaU = 0.d0 
+      U0 = 0.d0
+      Un = 0.d0
 
 c      atom-atom interactions
 
-      do js = 1,natoms
-         call lj(i,js,r0,box,rc,U0)
-         call lj(i, js, rn, box, rc, Un)
+      do js = 1,npart
+         if (js == iSel)  cycle
+         call lj(iSel, js, r0, box, rc, U0)
+         call lj(iSel, js, rn, box, rc, Un)
          deltaU = deltaU + Un - U0
       end do
 
@@ -128,17 +178,18 @@ c      atom-atom interactions
 c              subroutine totEnergy
 *********************************************************
 *********************************************************
-      subroutine totEnergy(natoms, r, rc, Utot)
-         implicit double precision(a-h,o-z)
-         dimension r(3,1000)
+      subroutine totEnergy(npart, r, box, rc, Utot)
+      implicit double precision(a-h,o-z)
+      dimension r(3,1000)
 c     This subroutine calculates the energy of a configuration
 
 
       Utot = 0.d0 
 
 c      atom-atom interactions
-      do is = 1,natoms-1
-         do js = is+1,natoms
+      do is = 1,npart-1
+c         print *, 'Atom ', is, ' coords = ', r(1,is), r(2,is), r(3,is)
+         do js = is+1,npart
             call lj(is,js,r,box,rc,Uij)
             Utot = Utot + Uij
          end do
@@ -155,15 +206,15 @@ c              subroutine Lennard-Jones
 *********************************************************
       subroutine lj(is,js,r,box,rc,Uij)
       implicit double precision(a-h,o-z)
-      dimension r(3,1000)
-      dimension rij(3)
+      dimension r(3,1000), rij(3)
 
       rr2 = 0.d0
-      pot = 0.d0
+      Uij = 0.d0
+      rijl = 0
       do l = 1,3
          rijl = r(l,js) - r(l,is)
          rij(l) = rijl - box*dnint(rijl/box)
-         rr2 = rr2 + rij(l)*rij(l)
+         rr2 = rr2 + rijl**2
       end do
 
       rr = dsqrt(rr2)
@@ -184,13 +235,13 @@ c              subroutine Lennard-Jones
 c              subroutine boundaryConds
 *********************************************************
 *********************************************************
-      subroutine boundaryConds(natoms, r, box)
+      subroutine boundaryConds(npart, r, box)
       implicit double precision(a-h,o-z)
       dimension r(3,1000)
 c     This subroutine applies periodic boundary conditions
 c     (box with a corner at the origin)
 
-      do i = 1, natoms
+      do i = 1, npart
          do l = 1,3
             if(r(l,i) < 0 ) r(l,i) = r(l,i) + box
             if(r(l,i) > 0 ) r(l,i) = r(l,i) - box
@@ -201,49 +252,3 @@ c     (box with a corner at the origin)
       return
       end
 
-*********************************************************
-*********************************************************
-c              subroutine moveparticle
-*********************************************************
-*********************************************************
-      subroutine moveparticle(natoms, r0, rc, deltax, beta, isuccess)
-      implicit double precision(a-h,o-z)
-      dimension r0(3,1000), rn(3,1000) 
-c     This subroutine choses a particle at random and attempts a move
-
-c     Notation: variable0 -> Old values
-c               variablen -> new attempted values
-
-        
-      ! Chose particle at random
-      ipart = 1 + int(rand()*npart)
-      if (ipart > npart) ipart = npart
-
-      do i = 1, npart
-         do l = 1,3
-            rn(l,i) = r0(l,i)
-         enddo
-      enddo
-
-      ! Move a particle at random
-      do l = 1,3
-         rn(l,i) = r0(l,i) + deltax*(rand()-0.d5)
-      enddo
-
-      ! Energy difference
-      call totEnergy(natoms, r0, rc, Utot0)
-      call totEnergy(natoms, rn, rc, Utotn)
-
-
-      acc = exp(-beta*(Utotn - Utot0))
-      print *, acc
-      if(rand() >= acc) then
-         do i = 1, npart
-            do l = 1,3
-               r0(l,i) = rn(l,i)
-            enddo
-         enddo
-         isuccess = isuccess + 1
-      endif ! We do nothing
-
-      end
